29cd5152d2887b5194640a29880b2dfa9abffe66
Vyacheslav Soldatov <vyacheslav.soldatov@inbox.ru>
2020-07-03 13:43:40 +0300

    Unit tests for dates and times

--- se.redfield.knime.neo4jextension.tests/src/main/java/se/redfield/knime/table/Neo4jTableOutputSupportTest.java
+++ se.redfield.knime.neo4jextension.tests/src/main/java/se/redfield/knime/table/Neo4jTableOutputSupportTest.java
@@ -7,14 +7,22 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.knime.core.data.DataCell;
+import org.knime.core.data.DataType;
 import org.knime.core.data.def.StringCell;
+import org.knime.core.data.time.duration.DurationCell;
+import org.knime.core.data.time.localdate.LocalDateCell;
+import org.knime.core.data.time.localdate.LocalDateCellFactory;
+import org.knime.core.data.time.localdatetime.LocalDateTimeCell;
+import org.knime.core.data.time.localdatetime.LocalDateTimeCellFactory;
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.Record;
 import org.neo4j.driver.Value;
@@ -75,7 +83,7 @@ public class Neo4jTableOutputSupportTest {
     }
     @Test
     public void testDuration() {
-        final List<Record> res = run("return datetime(\"2019-06-01T18:40:32.142+0100\") as dt");
+        final List<Record> res = run("return duration({minutes: 2, seconds: 30}) as dt");
         assertEquals(1, res.size());
 
         final Record rec = res.get(0);
@@ -88,9 +96,58 @@ public class Neo4jTableOutputSupportTest {
         assertNotNull(t.calculateType());
 
         //test value
-        final DataCell cell = support.createCell(t.calculateType(), v);
+        final DurationCell cell = (DurationCell) support.createCell(t.calculateType(), v);
         assertNotNull(cell);
+        assertEquals((2 * 60 + 30) * 1000l, cell.getDuration().toMillis());
     }
+    @Test
+    public void testDate() {
+        final String query = "UNWIND [\n" +
+                "    { created: date(\"2019-06-01\") }\n" +
+                "] AS t\n" +
+                "return t.created";
+
+        final List<Record> res = run(query);
+        assertEquals(1, res.size());
+
+        final Record r = res.get(0);
+        assertEquals(1, r.size());
+
+        final Value v = r.get(0);
+        final DataTypeDetection t = support.detectCompatibleCellType(v);
+
+        assertTrue(t.isDetected());
+        final DataType type = t.calculateType();
+        assertEquals(LocalDateCellFactory.TYPE, type);
+
+        final LocalDateCell cell = (LocalDateCell) support.createCell(type, v);
+        assertEqualsDates("2019-06-01", cell.getLocalDate());
+    }
+    @Test
+    public void testDateTime() {
+        final String query = "UNWIND [\n" +
+                "    { created: datetime(\"2019-06-01T18:40:32.142+0100\") }\n" +
+                "] AS t\n" +
+                "return t.created";
+
+        final List<Record> res = run(query);
+        assertEquals(1, res.size());
+
+        final Record r = res.get(0);
+        assertEquals(1, r.size());
+
+        final Value v = r.get(0);
+        final DataTypeDetection t = support.detectCompatibleCellType(v);
+
+        assertTrue(t.isDetected());
+        final DataType type = t.calculateType();
+        assertEquals(LocalDateTimeCellFactory.TYPE, type);
+
+        final LocalDateTimeCell cell = (LocalDateTimeCell) support.createCell(type, v);
+        assertEqualsDateTimes("2019-06-01T18:40:32.142+0100",
+                cell.getLocalDateTime());
+    }
+
     @Test
     public void testCollections() {
         final String query = "UNWIND [\n" +
@@ -122,7 +179,16 @@ public class Neo4jTableOutputSupportTest {
         assertTrue(t.isList());
         assertEquals(StringCell.TYPE, t.calculateType());
     }
-
+    private void assertEqualsDates(final String dateStr, final LocalDate d2) {
+        final DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        final LocalDate d1 = LocalDate.parse(dateStr, fmt);
+        assertEquals(d1, d2);
+    }
+    private void assertEqualsDateTimes(final String dateStr, final LocalDateTime d2) {
+        final DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+        final LocalDateTime d1 = LocalDateTime.parse(dateStr, fmt);
+        assertEquals(d1, d2);
+    }
     private List<Record> run(final String query) {
         return Neo4jSupport.runRead(driver, query, null);
     }
